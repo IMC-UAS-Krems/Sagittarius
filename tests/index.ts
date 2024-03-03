e8d4c1be41627eb200a050b587b41dbc8d8d24c0
bobokrut <egor-evlampiev@mail.ru>
2024-03-04 00:02:44 +0100

    added small test for testing entire workflow

---
+++ tests/index.ts
@@ -0,0 +1,108 @@
+import expect from "expect";
+import puppeteer from "puppeteer";
+
+const input = `
+service:
+    title is Grafana dashboard
+    version is 1.0.0
+    scope is Environment
+    test:
+        one is one
+        two is two
+
+data:
+    sources -> first
+
+first:
+    type is SmartMeter
+    provider is Fiware
+    uri is https://data.iiss.at/dataskop/fiwarenosec/v2/entities
+    query is AirQualityObserved
+
+application:
+    type is Web
+    layout is SinglePage
+    roles -> User, SuperUser, Admin
+    panels -> Map, Pie, XY, TS, Bar
+
+Map:
+    label is map
+    type is geomap
+    source is first
+    data -> location, stationName, O3, NO2, SO2, address
+
+Pie:
+    label is pie
+    type is pie_chart
+    source is first
+    traces -> NOx, O3, NO2, SO2
+    pie_chart_type is pie
+
+XY:
+    label is xy
+    type is xy_chart
+    source is first
+    traces -> dateObserved, NOx, O3, NO2, SO2
+
+TS:
+    label is ts
+    type is timeseries
+    source is first
+    traces -> dateObserved, NOx, O3, NO2, SO2
+
+Bar:
+    label is bar
+    type is bar_chart
+    source is first
+    traces -> dateObserved, NOx, O3, NO2, SO2
+
+deployment:
+    environments -> local
+
+local:
+    uri is https://localhost.org:3000/test
+    port is 50055
+    type is Docker
+`;
+
+const initBrowser = async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--disable-infobars', '--disable-notifications']
+    });
+    return browser;
+};
+
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+(async () => {
+    const browser = await initBrowser();
+    try {
+        const context = browser.defaultBrowserContext();
+        await context.overridePermissions("http://localhost", ['notifications']);
+        const page = await browser.newPage();
+
+        page.on('response', async (response) => {
+            if (response.url() === "http://localhost:9000/deploy" && response.request().method() === "POST") {
+                const responseData = await response.text();
+                expect(responseData).toMatch(/^"https:\/\/sag-[a-z0-9]+-grafana.azurewebsites.net"$/);
+                console.log("Deployment URL:", responseData);
+                browser.close();
+            }
+            expect(response.status()).toBeLessThan(400);
+        });
+
+        await page.goto("http://localhost");
+        await page.click('._headerMainContianer_1876s_1 > nav:nth-child(2) > ul:nth-child(1) > li:nth-child(4) > a:nth-child(1)');
+        await page.type('#textfield-cl-0-input', "email@example6.com");
+        await page.type('#textfield-cl-2-input', "password123");
+        await page.click("html body.lightMode div#root main._signinMainContainer_8otdj_1 div._signinCardContainer_8otdj_13 button");
+
+        await page.waitForSelector('html body.lightMode div#root main div.editor-container div.middle-column div div.cm-editor.ͼ1.ͼ2 div.cm-scroller div.cm-content');
+        await page.type("html body.lightMode div#root main div.editor-container div.middle-column div div.cm-editor.ͼ1.ͼ2 div.cm-scroller div.cm-content", input, { delay: 0 });
+        await page.click("#root > main > button");
+    } catch (error) {
+        console.error("Error occurred:", error);
+    }
+})();
+
