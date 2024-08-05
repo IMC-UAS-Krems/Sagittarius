f63caf34abf1f9e2352753a2c26b9b054f0679fe
bobokrut <egor-evlampiev@mail.ru>
2024-08-05 21:48:56 +0200

    chore: removed unneeded files

--- utils/init_database.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-npm install prisma @prisma/client
-# npx prisma db push --force-reset # reset database
-npx prisma migrate reset --force
-npx prisma generate
-node ./init_database.js
